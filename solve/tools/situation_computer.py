b4138b8e2945f43d12f708bb77f4efa3f99a2a2b
Alejandro Arias <jandroariass@gmail.com>
2024-01-29 14:37:32 +0000

    Change matrix name

--- solve/tools/situation_computer.py
+++ solve/tools/situation_computer.py
@@ -18,18 +18,18 @@ class SituationComputer:
 
     @staticmethod
     def compute_structure_situation() -> Tuple:
-        matriz_9x9 = np.arange(81).reshape(9, 9)
+        matrix_9x9 = np.arange(81).reshape(9, 9)
         row_situation = {}
         column_situation = {}
         box_situation = {}
 
         for i in range(9):
-            row_situation[i] = matriz_9x9[i, :]
-            column_situation[i] = matriz_9x9[:, i]
+            row_situation[i] = matrix_9x9[i, :]
+            column_situation[i] = matrix_9x9[:, i]
 
         for i in range(3):
             for j in range(3):
-                nine_box_cells = matriz_9x9[i * 3: (i + 1) * 3, j * 3: (j + 1) * 3]
+                nine_box_cells = matrix_9x9[i * 3: (i + 1) * 3, j * 3: (j + 1) * 3]
                 flattened_list = [item for sublist in nine_box_cells.tolist() for item in sublist]
                 box_situation[i * 3 + j] = flattened_list
 
